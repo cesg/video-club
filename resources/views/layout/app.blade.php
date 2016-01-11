7d28eadee989d41105f3ed4a8840d0211217ebe0
CristianSG <cesgbla@gmail.com>
2016-01-10 22:54:28 -0300

    View - layout : fix app layout

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel</title>
-    {!! Html::style('css/bootstrap.css') !!}
+    <link rel="stylesheet" href="{!! asset('css/bootstrap.css') !!}">
+
             <!-- Fonts -->
     <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->

f2ade12a9c742b896bb21f09200a91c6486b25eb
CristianSG <cesgbla@gmail.com>
2016-01-14 00:55:06 -0300

    Mejora en login

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.app')
 @section('content')
     <div class="container">
-        <h2>Hola : {{ \Auth::getUser()->name() }}</h2>
+        <h2>Hola : {{ Auth::user()->name }}</h2>
         <p></p>
     </div>
 @endsection

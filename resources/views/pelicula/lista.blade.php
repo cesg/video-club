4c1f571789d57f3a85cda24c8f5aeacb43c3ee61
CristianSG <cesgbla@gmail.com>
2016-01-15 13:32:39 -0300

    Mantenedor - Pelicula mejora en estilo en lista

--- resources/views/pelicula/lista.blade.php
+++ resources/views/pelicula/lista.blade.php
@@ -14,48 +14,56 @@
 @section('content')
     <div class="container" ng-controller="listaPeliculaCtr">
         <div class="row">
-            <h2>Buscar película por título</h2>
-            <a href="{!! url('/mantenedor/pelicula/crear') !!}">Agregar Compra</a>
-            <div id="custom-search-input">
-                <div class="input-group col-md-12">
-                    <input type="text" class="  search-query form-control" placeholder="Search" ng-model="titulo"/>
+            <div class="panel panel-default">
+                <div class="panel-heading">Mantenedor - Película</div>
+                <div class="panel-body">
+                    <h2>Buscar película por título</h2>
+                    <a href="{!! url('/mantenedor/pelicula/crear') !!}">Agregar Compra</a>
+                    <div id="custom-search-input">
+                        <div class="input-group col-md-12">
+                            <input type="text" class="  search-query form-control" placeholder="Search"
+                                   ng-model="titulo"/>
                         <span class="input-group-btn">
-                                    <button class="btn btn-danger" type="button" ng-click="buscarPeliculas()">
-                                        <span class=" glyphicon glyphicon-search"></span>
-                                    </button>
+                        <button class="btn btn-danger" type="button" ng-click="buscarPeliculas()">
+                            <span class=" glyphicon glyphicon-search"></span>
+                        </button>
                         </span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <table class="table">
-                <tr>
-                    <th>Titulo</th>
-                    <td>Acciones</td>
-                </tr>
-                <tr ng-repeat="pelicula in peliculas">
-                    <td>@{{ pelicula.titulo }}</td>
-                    <td><a href="{!! url('/mantenedor/pelicula/') !!}@{{ '/' + pelicula.id + '/editar' }}"><span class="glyphicon glyphicon-edit"></span> Editar</a></td>
-                </tr>
-            </table>
 
-        </div>
+    <div class="row" ng-show="resultados" class="ng-hide">
+        <table class="table">
+            <tr>
+                <th>Titulo</th>
+                <td>Acciones</td>
+            </tr>
+            <tr ng-repeat="pelicula in peliculas">
+                <td>@{{ pelicula.titulo }}</td>
+                <td><a href="{!! url('/mantenedor/pelicula/') !!}@{{ '/' + pelicula.id + '/editar' }}"><span
+                                class="glyphicon glyphicon-edit"></span> Editar</a></td>
+            </tr>
+        </table>
+
     </div>
 @endsection
 @section('scripts')
     <script href="{!! asset('js/app.js') !!}"></script>
     <script type="text/javascript">
-        app.controller('listaPeliculaCtr', function($scope, $http) {
+        app.controller('listaPeliculaCtr', function ($scope, $http) {
             $scope.peliculas = [];
-
+            $scope.resultados = false;
             $scope.buscarPeliculas = function () {
                 if ($scope.titulo) {
-                    $http.get('api/pelicula?titulo='+ $scope.titulo).success(function (data) {
+                    $http.get('api/pelicula?titulo=' + $scope.titulo).success(function (data) {
                         $scope.peliculas = data.peliculas;
                     });
                 } else {
                     $http.get('/api/pelicula').success(function (data) {
                         $scope.peliculas = data.peliculas;
+                        $scope.resultados = true;
                     });
                 }
             };
